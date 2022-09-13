b0c183f7da5c5014321441e95faa0be966e02065
Fagotto <joffrey.fagotto4@laposte.net>
2022-09-13 14:06:29 +0200

    Curvepoint controller/domain/repo

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -1,14 +1,62 @@
-//package com.nnk.springboot.domain;
-//
-//
-//
-//import javax.persistence.*;
-//
-//import java.sql.Timestamp;
-//
-//
-//@Entity
-//@Table(name = "curvepoint")
-//public class CurvePoint {
-//    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
-//}
+package com.nnk.springboot.domain;
+
+
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "curvepoint")
+public class CurvePoint implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name ="Id")
+    private Integer id;
+
+    @Column(name ="asOfDate")
+    private Date asOfDate;
+
+
+    @Column(name ="term")
+    private double term;
+
+    @Column(name ="creationDate")
+    private Date creationDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getAsOfDate() {
+        return asOfDate;
+    }
+
+    public void setAsOfDate(Date asOfDate) {
+        this.asOfDate = asOfDate;
+    }
+
+    public double getTerm() {
+        return term;
+    }
+
+    public void setTerm(double term) {
+        this.term = term;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
